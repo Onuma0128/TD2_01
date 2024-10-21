c7c4429b1e48bdfd518f26ec407e5e8bc5352c66
Onuma0128 <147121758+Onuma0128@users.noreply.github.com>
2024-10-21 21:34:01 +0900

    ゲームオーバー時のカメラの移動を追加、ゲームオーバーSpriteを追加

---
+++ project/Game/GameScene/GameOverCamera/GameOverCamera.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <memory>
+
+#include "Engine/Math/Vector2.h"
+#include "Engine/Math/Vector3.h"
+#include "Engine/Module/Transform2D/Transform2D.h"
+#include "Engine/Module/GameObject/SpriteObject.h"
+
+#include "Game/GlobalValues/GlobalValues.h"
+
+class Camera3D;
+class Player;
+
+class GameOverCamera
+{
+public:
+
+	enum class CameraState {
+		Move,
+		Stop,
+		GameOverSprite,
+	};
+
+	void initialize();
+
+	void update();
+
+	void begin_rendering();
+
+	void draw();
+
+	float easeOutQuint(float t);
+
+	void Move();
+
+	void Stop();
+
+	void SpriteMove();
+
+private:
+
+	CameraState state_;
+
+	Vector2 oldSpritePos_{};
+	Vector3 oldPos_{};
+	Vector3 playerPos_{};
+
+	float cameraFrame_ = 0;
+	bool isMoveCamera_ = false;
+
+	std::unique_ptr<SpriteObject> gameOverSprite_ = nullptr;
+
+public:
+
+	inline static GlobalValues& globalValues = GlobalValues::GetInstance();
+	inline static Camera3D* camera3d_ = nullptr;
+	inline static Player* player_ = nullptr;
+
+};
--- project/Game/GameScene/GameScene.h
+++ project/Game/GameScene/GameScene.h
@@ -7,6 +7,7 @@
 #include "Engine/Module/Camera/Camera2D.h"
 #include "Engine/Module/Camera/Camera3D.h"
 #include "Engine/Module/Collision/CollisionManager.h"
+
 #include "Game/GameScene/Player/Player.h"
 #include "Game/GameScene/Player/PlayerHPManager.h"
 #include "Game/GameScene/Player/PlayerSweat.h"
@@ -15,6 +16,7 @@
 #include "Game/GameScene/GameUI/UIManager.h"
 #include "Game/GameScene/EnemyManager/EnemyManager.h"
 #include "Game/GameScene/RenderNode/CircleGauge/CircleGaugeNode.h"
+#include "Game/GameScene/GameOverCamera/GameOverCamera.h"
 
 #ifdef _DEBUG
 #include "Game/GameScene/Timeline/TimelineEditor/TimelineEditor.h"
@@ -53,6 +55,8 @@ private:
 	std::unique_ptr<EnemyManager> enemyManager = nullptr;
 
 	std::unique_ptr<Camera3D> camera3D_ = nullptr;
+	std::unique_ptr<GameObject> ground_ = nullptr;
+	std::unique_ptr<GameOverCamera> gameOverCamera_ = nullptr;
 	std::unique_ptr<Player> player_ = nullptr;
 	std::unique_ptr<PlayerHPManager> playerHpManager_ = nullptr;
 	std::unique_ptr<UIManager> uiManager_ = nullptr;
